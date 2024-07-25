96bd87acf5e085173c0d5cf7c7001e5476ca17f5
VlahdimyrLB <bambalanvlahdimyr@gmail.com>
2024-07-25 17:16:31 +0800

    finished stacks

---
+++ CodingTopics/stacks.js
@@ -0,0 +1,73 @@
+// STACK - last In, First Out (LIFO)
+// functions: push, pop, peek, length
+
+// Array Stack - since arrays have all the funciton of a stack
+// simplex example to understand stacks with palindrome
+
+let letters = []; // this is the array stack
+let word = "racecar";
+let reversedWord = "";
+
+// put letter words into stack
+for (let i = 0; i < word.length; i++) {
+  letters.push(word[i]);
+}
+
+// pop off the stack in reverse order
+for (let i = 0; i < word.length; i++) {
+  reversedWord += letters.pop();
+}
+
+if (word === reversedWord) {
+  console.log(word + " is a plaindrome ");
+} else {
+  console.log(word + " is not a plaindrome ");
+}
+
+// ACTUAL STACK using ES6 Class
+class Stack {
+  constructor() {
+    this.count = 0;
+    this.storage = {};
+  }
+
+  // Adds a value onto the end of stack
+  push(value) {
+    this.storage[this.count] = value;
+    this.count++;
+  }
+
+  // removes and returns the value at the end of the stack
+  pop() {
+    if (this.count === 0) {
+      return undefined;
+    }
+
+    this.count--;
+    let result = this.storage[this.count];
+    delete this.storage[this.count];
+    return result;
+  }
+
+  size() {
+    return this.count;
+  }
+
+  // return the value at the end of stack
+  peek() {
+    return this.storage[this.count - 1];
+  }
+}
+
+let myStack = new Stack();
+
+myStack.push(1);
+myStack.push(2);
+console.log(myStack.peek()); // 2
+console.log(myStack.pop()); // 2
+console.log(myStack.peek()); // 1
+
+myStack.push("Vlahd learns stacks xd");
+
+console.log(myStack.size()); // 2
+console.log(myStack.peek()); // Vlahd learns stacks xd
---
+++ algorithms.js
@@ -0,0 +1,31 @@
+/*
+ALGORITHMS - An algorithm is set of well-defined instructions to solve a particular problem.
+
+    ex. Algorithm to add two numbers
+
+    Inputs: Two numbers, a and b
+    Algorithm: Add number using + sign and return the value
+    Output: Sum of a and b
+
+
+    Characteristics: 
+        - Well-defined input and output
+        - each step should clear
+*/
+
+/*
+ALGORITHM ANALYSIS
+
+1. Time complexity - amount of time taken by an algorithm to run, as a function of input size
+2. Space comlexity - amount of memory taken by an algorithm to run, as a function of input size
+
+How to represent? 
+    use Asymptotic notations (mathematical tools to represent time and complexity)
+        1. Big O Notation - Worst Case Complexity (/)
+        2. Omega Notation - Best Case Complexity
+        3. Theta Notation - Average Case Complexity
+
+
+    BIG O NOTATION 
+        describes the complexity of an algorithm using algebraic terms
+*/
